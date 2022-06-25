a42bbb78d2934d130417444c43b07ee3945ff347
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2022-06-25 16:10:51 -0400

    Remove pkam auth (`withRemoteSecondary` already PKAM auths)

--- at_client/src/main/java/org/atsign/client/cli/Scan.java
+++ at_client/src/main/java/org/atsign/client/cli/Scan.java
@@ -52,45 +52,12 @@ public class Scan {
             System.exit(1);
         }
         
-
-        // initialize AtClient and connect to remote secondary
+        // initialize AtClient and connect to remote secondary (with pkam auth)
         AtClient atClient = null;
         try {
             what = "initialize AtClient";
-            atClient = AtClient.withRemoteSecondary(atSign, ArgsUtil.createAddressFinder(rootUrl));
-        } catch (AtException | IOException e) {
-            System.err.println("Failed to " + what + " " + e.getMessage());
-            e.printStackTrace(System.err);
-            System.exit(1);
-        }
-
-        // pkam auth
-        try {
-            System.out.println("Starting PKAM");
-            AuthUtil authUtil = new AuthUtil();
-            // 1. get secondary server address 
-            what = "look up at sign via new AtRootConnection(" + rootUrl + ").lookupAtSign(" + atSign.atSign + ");";
-            String secondaryUrl = new AtRootConnection(rootUrl).lookupAtSign(atSign);
-            System.out.println("Retrieved Secondary URL of " + atSign.atSign + ": " + secondaryUrl);
-            
-            // 2. initialize connection
-            AtSecondaryConnection conn = new AtSecondaryConnection(new SimpleAtEventBus(), atSign, secondaryUrl, null, false, false);
-
-            // 3. establish connection
-            what = "establish connection to secondary in PKAM auth process.";
-            conn.connect();
-            System.out.println("Initialized connection: " + conn.getUrl());
-
-            // 4. load .atKeys to get pkam private key
-            what = "load keys from atSign: " + atSign.atSign + ".";
-            Map<String, String> keys = KeysUtil.loadKeys(atSign);
-            System.out.println("Successfully retrieved .atKeys.");
-
-            // 5. use pkam private key to encrypt challenge
-            what= "PKAM authenticate";
-            authUtil.authenticateWithPkam(conn, atSign, keys);
-            System.out.println("PKAM Auth Success");
-        } catch (Exception e) {
+            atClient = AtClient.withRemoteSecondary(atSign, sAddress, verbose);
+        } catch (AtException e) {
             System.err.println("Failed to " + what + " " + e.getMessage());
             e.printStackTrace(System.err);
             System.exit(1);
