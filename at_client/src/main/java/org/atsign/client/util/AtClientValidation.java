22e21ed415e08ecf71c15b267a4c0207f4b05faa
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2022-08-16 08:37:13 -0400

    feat: `AtClientValidation.java`

---
+++ at_client/src/main/java/org/atsign/client/util/AtClientValidation.java
@@ -0,0 +1,119 @@
+package org.atsign.client.util;
+
+import java.io.IOException;
+
+import org.atsign.client.api.Secondary;
+import org.atsign.common.AtException;
+import org.atsign.common.AtSign;
+import org.atsign.common.Keys.AtKey;
+import org.atsign.common.Keys.Metadata;
+
+public class AtClientValidation {
+
+    /**
+     * @param keyName // e.g. "test" (not the fullKeyName like "public:test@bob")
+     * @throws AtException
+     */
+    public static void validateKeyName(String keyName) throws AtException {
+        // key cannot be null and cannot be the empty string (length 0)
+        if (keyName == null || keyName.isEmpty()) {
+            throw new AtException("Key cannot be null or empty");
+        }
+
+        // key cannot have spaces
+        if (keyName.contains(" ")) {
+            throw new AtException("Key cannot have spaces");
+        }
+
+        // Key cannot contain @
+        if (keyName.contains("@")) {
+            throw new AtException("Key cannot contain @");
+        }
+    }
+
+    /**
+     * 
+     * @param metadata Metadata object to validate
+     * @throws AtException if metadata is null or has invalid values
+     */
+    public static void validateMetadata(Metadata metadata) throws AtException {
+        // null check
+        if (metadata == null) {
+            throw new AtException("Metadata cannot be null");
+        }
+
+        // validate ttl
+        if (metadata.ttl == null || (metadata.ttl < 0)) {
+            throw new AtException("Ttl cannot be null and cannot be negative");
+        }
+
+        // validate ttb
+        if (metadata.ttb == null || metadata.ttb < 0) {
+            throw new AtException("Ttb cannot be null and cannot be negative");
+        }
+
+        // validate ttr
+        if (metadata.ttr == null || metadata.ttr < -1) {
+            throw new AtException("Ttb cannot be null and cannot be < -1");
+        }
+
+    }
+
+    /**
+     * Checks if an atSign exists on a root server
+     * 
+     * @param atSign  atSign object to check for existence
+     * @param rootUrl rootUrl of the root server for atSign existence
+     * @throws AtException if atSign does not exist/address could not be found, if
+     *                     atSign object is empty,
+     */
+    public static void atSignExists(AtSign atSign, String rootUrl) throws AtException {
+        if(atSign == null) {
+            throw new AtException("atSign cannot be null");
+        }
+        if (atSign.toString().isEmpty()) {
+            throw new AtException("atSign cannot be empty");
+        }
+        Secondary.AddressFinder finder = ArgsUtil.createAddressFinder(rootUrl);
+        try {
+            finder.findSecondary(atSign);
+        } catch (IOException e) {
+            throw new AtException("Could not find secondary of atSign: " + atSign.toString());
+        }
+    }
+
+    public static void atSignExists(AtSign atSign, String rootDomain, String rootPort) throws AtException {
+        atSignExists(atSign, rootDomain + ":" + rootPort);
+    }
+
+    /**
+     * Validate if an AtKey object is valid
+     * 1. checks if atKey.name is a valid keyName
+     * 2. checks if atKey.metadata has valid values
+     * 3. checks if atKey.sharedWith atSign is valid (if it exists)
+     * 
+     * @param atKey   AtKey object created usually through KeyBuilders
+     * @param rootUrl RootURL of the Root Server (e.g. "root.atsign.org:64")
+     * @throws AtException if the AtKey is invalid
+     */
+    public static void validateAtKey(AtKey atKey, String rootUrl) throws AtException {
+
+        // 1. null check
+        if(atKey == null) {
+            throw new AtException("AtKey cannot be null");
+        }
+
+        // 2. validate key name
+        validateKeyName(atKey.name);
+
+        // 3. validate metadata
+        validateMetadata(atKey.metadata);
+
+        // 4. validate sharedWith exists
+        if (atKey.sharedWith != null) {
+            atSignExists(atKey.sharedWith, rootUrl);
+        }
+
+    }
+
+}
