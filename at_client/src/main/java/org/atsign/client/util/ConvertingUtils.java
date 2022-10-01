864eff47cbfab6b5e3b978e1c7ed90b3488a9fcd
Yash Jain <69360295+yashjain1974@users.noreply.github.com>
2022-10-01 16:10:21 +0530

    Added new java file
    This java file has two methods.
    1) public static byte[] convert(File);   //to convert byte array to file
    2)public static File convert(byte[]);  //to convert File to byte array

---
+++ at_client/src/main/java/org/atsign/client/util/ConvertingUtils.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+// Main class
+
+public class ConvertingUtils {
+
+     // Converting file object to byte array
+    public static byte[] convert(File file)
+        throws IOException
+    {
+  
+        //creating an instance of file
+        FileInputStream filel = new FileInputStream(file);
+  
+        //creating a byte array with same length as file
+        byte[] byteArr = new byte[(int)file.length()];
+  
+       //Reading file content 
+        filel.read(byteArr);
+  
+        //Closing the file
+        filel.close();
+  
+        // Returning byte array
+        return byteArr;
+    }
+
+  
+  //Converting byte array to file
+    public static File convert(byte[] bytes)
+    {
+        //paste yout destination path here
+        String FILEPATH="";  
+        File file = new File(FILEPATH);
+       
+        try {
+ 
+            // InitializING a pointer in file
+            OutputStream ostream = new FileOutputStream(file);
+            
+            // Starting writing the bytes in it
+            ostream.write(bytes);
+            System.out.println("Successfully byte inserted");
+ 
+            ostream.close();
+         
+        }
+ 
+        // Catch block to handle the exceptions
+        catch (Exception e) {
+ 
+            // Display exception on console
+            System.out.println("Exception: " + e);
+        }
+        return file;
+    }
+}
