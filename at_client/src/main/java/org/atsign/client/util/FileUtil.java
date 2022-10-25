0eac1d82463c1eb571788a7a59fb64fee35ed06e
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2022-10-25 15:45:19 -0400

    refactor: fix spacing and method names

--- at_client/src/main/java/org/atsign/client/util/FileUtil.java
+++ at_client/src/main/java/org/atsign/client/util/FileUtil.java
@@ -1,12 +1,11 @@
 package org.atsign.client.util;
 
 import java.io.*;
-public class FileUtil
-{
+
+public class FileUtil {
 	
 	// This method converts a file to a byte array and returns the byte array
-	public static byte[] fileToByteArray(File file)throws IOException
-	{
+	public static byte[] convert(File file) throws IOException {
 		FileInputStream fis = new FileInputStream(file);
         byte[] data = new byte[(int)file.length()];
         fis.read(data);
@@ -15,8 +14,7 @@ public class FileUtil
 	}
 	
 	// This method converts a byte array to a file and returns the file
-	public static File byteArrayToFile(byte data[], String fileName)throws IOException
-	{
+	public static File convert(byte data[], String fileName) throws IOException {
 		File file = new File(fileName);
 		OutputStream os = new FileOutputStream(file);
         os.write(data);
