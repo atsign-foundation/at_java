80261be8d77904f27095ab728d70ed0662bf0e29
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2022-11-01 13:42:49 -0400

    chore: add unadded import

--- at_client/src/main/java/org/atsign/client/api/AtClient.java
+++ at_client/src/main/java/org/atsign/client/api/AtClient.java
@@ -5,6 +5,7 @@ import org.atsign.client.api.impl.connections.AtRootConnection;
 import org.atsign.client.api.impl.connections.DefaultAtConnectionFactory;
 import org.atsign.client.api.impl.events.SimpleAtEventBus;
 import org.atsign.client.api.impl.secondaries.RemoteSecondary;
+import org.atsign.client.api.notification.NotificationService;
 import org.atsign.common.AtSign;
 import org.atsign.common.AtException;
 import org.atsign.client.util.KeysUtil;
@@ -161,4 +162,6 @@ public interface AtClient extends Secondary, AtEvents.AtEventBus {
     CompletableFuture<String> put(PublicKey publicKey, byte[] value);
 
     CompletableFuture<List<AtKey>> getAtKeys(String regex);
+
+    NotificationService getNotificationService();
 }
