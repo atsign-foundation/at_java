1f3298e7c7c7c76b72a05e46275318530c198ed0
TylerTrott <tylertrott24@yahoo.com>
2022-11-09 18:19:26 -0500

    Refactoring KeysUtil.java

--- at_client/src/main/java/org/atsign/client/util/KeysUtil.java
+++ at_client/src/main/java/org/atsign/client/util/KeysUtil.java
@@ -18,20 +18,26 @@ import java.util.TreeMap;
 public class KeysUtil {
     private static final ObjectMapper mapper = new ObjectMapper();
 
-    private static final String rootFolder = System.getProperty("user.home") + "/.atsign/keys/";
+    private static String rootFolder = System.getProperty("user.dir") + "/keys/";
 
-    public  static final String pkamPublicKeyName = "aesPkamPublicKey";
-    public  static final String pkamPrivateKeyName = "aesPkamPrivateKey";
-    public  static final String encryptionPublicKeyName = "aesEncryptPublicKey";
-    public  static final String encryptionPrivateKeyName = "aesEncryptPrivateKey";
-    public  static final String selfEncryptionKeyName = "selfEncryptionKey";
+    public static final String pkamPublicKeyName = "aesPkamPublicKey";
+    public static final String pkamPrivateKeyName = "aesPkamPrivateKey";
+    public static final String encryptionPublicKeyName = "aesEncryptPublicKey";
+    public static final String encryptionPrivateKeyName = "aesEncryptPrivateKey";
+    public static final String selfEncryptionKeyName = "selfEncryptionKey";
 
     public static void saveKeys(AtSign atSign, Map<String, String> keys) throws Exception {
         _makeRootFolder();
-        File file = getKeysFile(atSign);
+        File file = getKeysFile(atSign, rootFolder);
         System.out.println("Saving keys to " + file.getAbsolutePath());
-        if (file.exists()) {
-            //noinspection ResultOfMethodCallIgnored
+        // if keys do not exist in project dir, search in ~/
+        if (!file.exists()) {
+            rootFolder = System.getProperty("user.home") + "/.atsign/keys/" + atSign + "_key.atKeys";
+            _makeRootFolder();
+            file = getKeysFile(atSign, rootFolder);
+            System.out.println("Saving keys to " + file.getAbsolutePath());
+        } else {
+            // noinspection ResultOfMethodCallIgnored
             file.delete();
         }
 
@@ -39,46 +45,57 @@ public class KeysUtil {
 
         Map<String, String> encryptedKeys = new TreeMap<>();
 
-        // We encrypt all the keys with the AES self encryption key (which is left unencrypted)
+        // We encrypt all the keys with the AES self encryption key (which is left
+        // unencrypted)
         encryptedKeys.put(selfEncryptionKeyName, selfEncryptionKey);
-        encryptedKeys.put(pkamPublicKeyName, EncryptionUtil.aesEncryptToBase64(keys.get(pkamPublicKeyName), selfEncryptionKey));
-        encryptedKeys.put(pkamPrivateKeyName, EncryptionUtil.aesEncryptToBase64(keys.get(pkamPrivateKeyName), selfEncryptionKey));
-        encryptedKeys.put(encryptionPublicKeyName, EncryptionUtil.aesEncryptToBase64(keys.get(encryptionPublicKeyName), selfEncryptionKey));
-        encryptedKeys.put(encryptionPrivateKeyName, EncryptionUtil.aesEncryptToBase64(keys.get(encryptionPrivateKeyName), selfEncryptionKey));
+        encryptedKeys.put(pkamPublicKeyName,
+                EncryptionUtil.aesEncryptToBase64(keys.get(pkamPublicKeyName), selfEncryptionKey));
+        encryptedKeys.put(pkamPrivateKeyName,
+                EncryptionUtil.aesEncryptToBase64(keys.get(pkamPrivateKeyName), selfEncryptionKey));
+        encryptedKeys.put(encryptionPublicKeyName,
+                EncryptionUtil.aesEncryptToBase64(keys.get(encryptionPublicKeyName), selfEncryptionKey));
+        encryptedKeys.put(encryptionPrivateKeyName,
+                EncryptionUtil.aesEncryptToBase64(keys.get(encryptionPrivateKeyName), selfEncryptionKey));
 
         String json = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(encryptedKeys);
         Files.write(file.toPath(), json.getBytes(StandardCharsets.UTF_8));
     }
 
-    private static File getKeysFile(AtSign atSign) {
-        return new File(System.getProperty("user.home") + "/.atsign/keys/" + atSign + "_key.atKeys");
+    private static File getKeysFile(AtSign atSign, String rootFolder) {
+        return new File(rootFolder);
     }
 
     public static Map<String, String> loadKeys(AtSign atSign) throws Exception {
         _makeRootFolder();
-        File file = getKeysFile(atSign);
-        if (! file.exists()) {
+        File file = getKeysFile(atSign, rootFolder);
+        if (!file.exists()) {
             throw new AtException("loadKeys: No file at " + file.getAbsolutePath());
         }
 
         String json = new String(Files.readAllBytes(file.toPath()), StandardCharsets.UTF_8);
-        @SuppressWarnings("unchecked") Map<String, String> encryptedKeys = mapper.readValue(json, Map.class);
+        @SuppressWarnings("unchecked")
+        Map<String, String> encryptedKeys = mapper.readValue(json, Map.class);
 
-        // All the keys are encrypted with the AES self encryption key (which is left unencrypted)
+        // All the keys are encrypted with the AES self encryption key (which is left
+        // unencrypted)
         String selfEncryptionKey = encryptedKeys.get(selfEncryptionKeyName);
 
         Map<String, String> keys = new HashMap<>();
         keys.put(selfEncryptionKeyName, selfEncryptionKey);
-        keys.put(pkamPublicKeyName, EncryptionUtil.aesDecryptFromBase64(encryptedKeys.get(pkamPublicKeyName), selfEncryptionKey));
-        keys.put(pkamPrivateKeyName, EncryptionUtil.aesDecryptFromBase64(encryptedKeys.get(pkamPrivateKeyName), selfEncryptionKey));
-        keys.put(encryptionPublicKeyName, EncryptionUtil.aesDecryptFromBase64(encryptedKeys.get(encryptionPublicKeyName), selfEncryptionKey));
-        keys.put(encryptionPrivateKeyName, EncryptionUtil.aesDecryptFromBase64(encryptedKeys.get(encryptionPrivateKeyName), selfEncryptionKey));
+        keys.put(pkamPublicKeyName,
+                EncryptionUtil.aesDecryptFromBase64(encryptedKeys.get(pkamPublicKeyName), selfEncryptionKey));
+        keys.put(pkamPrivateKeyName,
+                EncryptionUtil.aesDecryptFromBase64(encryptedKeys.get(pkamPrivateKeyName), selfEncryptionKey));
+        keys.put(encryptionPublicKeyName,
+                EncryptionUtil.aesDecryptFromBase64(encryptedKeys.get(encryptionPublicKeyName), selfEncryptionKey));
+        keys.put(encryptionPrivateKeyName,
+                EncryptionUtil.aesDecryptFromBase64(encryptedKeys.get(encryptionPrivateKeyName), selfEncryptionKey));
         return keys;
     }
 
     private static void _makeRootFolder() throws IOException {
         Path dir = Paths.get(rootFolder);
-        if (! Files.exists(dir)) {
+        if (!Files.exists(dir)) {
             Files.createDirectories(dir);
         }
     }
